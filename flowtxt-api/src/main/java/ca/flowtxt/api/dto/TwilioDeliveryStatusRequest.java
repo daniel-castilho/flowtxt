0cbf22bc4e0c43e1c1f25eed7f6f0222f3b432d7
daniel-castilho <dan.castilho@gmail.com>
2025-11-05 00:45:19 -0500

    feat(api): adiciona DTO para webhook de status
    - Cria DTO para requisição do Twilio
    - Atualiza configurações da aplicação

--- flowtxt-api/src/main/java/ca/flowtxt/api/dto/TwilioDeliveryStatusRequest.java
+++ flowtxt-api/src/main/java/ca/flowtxt/api/dto/TwilioDeliveryStatusRequest.java
@@ -1,4 +1,15 @@
 package ca.flowtxt.api.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TwilioDeliveryStatusRequest {
+    private String MessageSid;
+    private String MessageStatus;
+    private String To;
+    private String From;
+    private String SmsStatus;
 }
+
